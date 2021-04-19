103071c1b08a50e4525853f8741ba9c8ae70454c
William Giani <holtzs.william@gmail.com>
2021-04-18 22:06:33 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,111 @@
+var fs = require("fs");
+const yargs = require("yargs");
+
+const options = yargs.usage("Usage: -n <name>").option("n", {
+  alias: "name",
+  describe: "Name of the component",
+  type: "string",
+  demandOption: true,
+});
+// .option("js", {
+//   alias: "javascript",
+//   describe: "Create component with javascript instead of typescript",
+//   type: "string",
+// }).argv;
+
+const capitalizeFirstLetter = (string) =>
+  string.charAt(0).toUpperCase() + string.slice(1);
+const createFiles = ({ name }) => {
+  let dir = "./components/";
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+
+  if (/.*\//.test(name)) {
+    let newDir = /.*\//.exec(name)[0];
+    dir = newDir[0] === "/" ? "./components" + newDir : dir + newDir;
+    name = /\/([a-zA-Z0-9]+)$/.exec(name)[1];
+  }
+
+  if (!fs.existsSync(`${dir}${name}`)) {
+    fs.mkdirSync(`${dir}${name}`, { recursive: true });
+  } else {
+    return console.error(
+      "\nOoops, something went wrong: \nThe component already exists, delete the folder or create with another name.\n"
+    );
+  }
+
+  fs.writeFile(
+    `${dir}${name}/index.ts`,
+    `export { default } from './${name}'
+export { ${name}Props } from './interfaces'
+`,
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+
+  fs.writeFile(
+    `${dir}${name}/interfaces.ts`,
+    `export interface ${name}Props {
+
+}
+`,
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+
+  fs.writeFile(
+    `${dir}${name}/${name}.tsx`,
+    `
+import React from 'react';
+import { ${name}Props } from '.'
+import s from "./${name}.module.css";
+
+
+const ${capitalizeFirstLetter(name)}: React.FC<${name}Props> = ({}) => {
+    return(
+        <>
+            
+        </>
+    )
+}
+
+export default ${capitalizeFirstLetter(name)};
+`,
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+
+  fs.writeFile(
+    `${dir}${name}/${name}.module.css`,
+    `.root {
+
+}
+`,
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
+
+  return console.log(
+    `\nSuccess: \nThe component ${name} was created successfully.\n`
+  );
+};
+
+createFiles(options);
